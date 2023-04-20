5008029858546d44fe0db546c33bdaa0631810e5
Ignacio <ivaldebenito52@gmail.com>
2023-04-19 21:04:04 -0400

    testeo

---
+++ src/main/java/org/example/Automovil.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Automovil {
+    //CRACION DEL OBJETO "NOMBRE DE LA CLASE"+"NOMBRE DEL OBJETO"+ =new "CLASE"();
+    Automovil auto1= new Automovil();
+    String marca;
+    String color;
+    String año;
+    String modelo;
+    int velocidad;
+
+
+}
